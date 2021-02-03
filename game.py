b4f46f4b000783c641c634a7745f1a60a6351630
sondejste <77987453+sondejste@users.noreply.github.com>
2021-02-03 17:40:34 -0500

    User input (choice)

--- game.py
+++ game.py
@@ -1,2 +1,38 @@
 # game
-print ("Rock, Paper, Scissors, Shoot!")
+#print ("Rock, Paper, Scissors, Shoot!")
+#^this was just a placeholder for setup
+
+
+print ("-------------------")
+print ("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print ("-------------------")
+
+#
+#asking user for input
+#
+
+x = input("Please choose either 'rock', 'paper', or 'scissors': ")
+
+
+print (f"You chose: {x}")
+
+#ways to format:
+#just printing: print("You chose:", x)
+#string concatonation: print("You chose:" + x)
+#string interpolation: print(f"You chose: {x})
+
+exit()
+
+print ("The computer chose: 'paper'")
+
+
+
+#simulating computer input
+
+print ("-------------------")
+print ("Oh, the computer won. It's ok.")
+
+#determining who won
+print ("-------------------")
+
+print ("Thanks for playing. Please play again!")
