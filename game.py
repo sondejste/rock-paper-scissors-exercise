9656dc064f0c980c18c7532eab93075b304d2788
sondejste <77987453+sondejste@users.noreply.github.com>
2021-02-08 01:17:57 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,12 +1,31 @@
 # game
-#print ("Rock, Paper, Scissors, Shoot!")
-#^this was just a placeholder for setup
 
 import random
 #makes the random module and all of its parts accessible to us
 #could also only import the function we need from the random module, in this case "random.choice"
 
+import dotenv
+dotenv.load_dotenv()
+#loads the dotenv module
 #add user input for username/name
+
+#creating a username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#beginning gameplay
 
 print ("-------------------")
 print ("Welcome 'Player One' to my Rock-Paper-Scissors game...")
@@ -51,8 +70,6 @@ print(f"The computer chose: {computer_choice}")
 
 print ("-------------------")
 
-
-
 #determining a winner
 
 if computer_choice == user_choice.lower(): 
@@ -60,6 +77,9 @@ if computer_choice == user_choice.lower():
     #double "==" is used because a single "=" is used to set variables equal to values. The double "==" needs to be used for equality operations/checking (think "is whats on the left, the same as whats on the right?")
 
 #the elif function is the same as elseif in other programs. This method is one of several correct options. Others include nested if statements.
+
+#this section has been adapted from Will Perrone's solution shared in Slack
+
 elif user_choice.lower() == "paper" and computer_choice == "rock":
         print("You win! Congrats!")
 elif user_choice.lower() == "paper" and computer_choice == "scissors":
